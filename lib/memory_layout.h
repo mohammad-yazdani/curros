c2047ea81817052f5f95d36b71cce93123c773c5
Mohammad Yazdani <yazdani.mohammad106@live.co.uk>
2018-11-17 21:27:47 -0500

    memory layout header

---
+++ lib/memory_layout.h
@@ -0,0 +1,9 @@
+/* System memory layout */
+
+#define user_space 0x0
+#define non_can 0x00007FFFFFFFFFFF
+#define kernel_space 0xFFFF800000000000
+#define mem_mapped_io 0xFFFF810000000000
+#define kernel_dynamic 0xFFFFFFFF80000000
+#define kernel_image 0xFFFFFFFFFFFFFFFF
+
